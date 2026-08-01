b1ebb96ca69ab13021fe1d9cac002dbc41d9a318
Kaustubh Ladiya <k4ustu3h@gmail.com>
2026-08-01 14:46:14 +0530

    Release v1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ val ciRunNumber = providers.environmentVariable("GITHUB_RUN_NUMBER").orNull.orEm
 val isReleaseBuild = ciBuild && (ciRef.contains("main") || ciRef.startsWith("refs/tags/"))
 val devReleaseName = if (ciBuild) "(Dev #$ciRunNumber)" else "($buildCommit)"
 
-val version = "1.1.1"
+val version = "1.2.0"
 val versionDisplayName = version + if (!isReleaseBuild) " $devReleaseName" else ""
 
 android {
@@ -30,7 +30,7 @@ android {
         applicationId = "k4ustu3h.monocons"
         minSdk = 26
         targetSdk = compileSdk
-        versionCode = 8
+        versionCode = 9
         versionName = versionDisplayName
         vectorDrawables.useSupportLibrary = true
     }
